80d77734fe6e7a1d26e670cf3b04cd4f9cd067db
dasamith <60905198+dasamith@users.noreply.github.com>
2020-08-12 13:11:03 -0400

    delete idea text

---
+++ client/build/precache-manifest.db2517afcd45c76116f61cf365760aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8356613e12c3472be116",
+    "url": "/static/css/main.e0f8c3ee.chunk.css"
+  },
+  {
+    "revision": "8356613e12c3472be116",
+    "url": "/static/js/main.a5f779f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b8aad7b706952c92d225",
+    "url": "/static/js/2.a1ae2c29.chunk.js"
+  },
+  {
+    "revision": "bba285234906d13459b57955ab9981fe",
+    "url": "/static/media/towel1.bba28523.jpg"
+  },
+  {
+    "revision": "0bd133a8c8378b682fd328afed9f89ea",
+    "url": "/index.html"
+  }
+];
